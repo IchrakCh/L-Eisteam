355d0c3c4458c1d507ed449f419bdd8bb8ee32ef
Hadrien-Abitbol <abitbolhad@cy-tech.fr>
2022-03-29 20:39:48 +0200

    ParsingFileDataClient

--- ParsingFile.py
+++ ParsingFile.py
@@ -1,4 +1,6 @@
 import pandas as pd
 DataJobbeur = pd.read_excel(r"/home/hadrien//ProjetTatami/L-Eisteam/DATAJOBEUR.xlsx",skiprows=[0,1,2,3,4,5])
 #print(DataJobbeur)
-#print(DataJobbeur.iloc[[998]])
+DataClient = pd.read_excel(r"/home/hadrien//ProjetTatami/L-Eisteam/DATACLIENT.xlsx",skiprows=[0,1,2,3,4,507,506])
+#print(DataJobbeur.iloc[[0]])
+#print(DataClient.iloc[[0]])
