790ef29b040e60ad1e08f28958b64e4627c1f262
Hadrien-Abitbol <abitbolhad@cy-tech.fr>
2022-03-29 20:25:48 +0200

    PremierCommit

---
+++ ParsingFile.py
@@ -0,0 +1,4 @@
+import pandas as pd
+DataJobbeur = pd.read_excel(r"/home/hadrien//ProjetTatami/L-Eisteam/DATAJOBEUR.xlsx",skiprows=[0,1,2,3,4,5])
+#print(DataJobbeur)
+#print(DataJobbeur.iloc[[998]])
