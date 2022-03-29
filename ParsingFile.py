bd3da06fb4b3b5a1298161a52f32e8fbaaa1d3ba
Hadrien-Abitbol <abitbolhad@cy-tech.fr>
2022-03-29 21:38:41 +0200

    ToLowerCaseALlDataSet

--- ParsingFile.py
+++ ParsingFile.py
@@ -11,6 +11,10 @@ DataClient = DataClient.loc[:, ~DataClient.columns.isin(['code NAF ', 'siret de 
 #print(DataClient.iloc[[0]])
 #DataJobbeur.to_csv('test1.csv')
 DataJobbeur = DataJobbeur.loc[:, ~DataJobbeur.columns.isin(['NOM','Prénom','CODE POSTAL','Permis PL','Caces 1','Caces 2','Caces 3','Connaissez vous la différence entre ces types de contrats ?','Si non, souhaitez-vous vous plus d\'informations ?','Si vous avez un profil Linkedin ou un e-CV, coller le lien URL :','Si vous avez un site internet, blog ou portfolio coller le lien URL'])]
-#DataJobbeur.to_csv('test2.csv')
 
+
+DataJobbeur = pd.concat([DataJobbeur[col].astype(str).str.lower() for col in DataJobbeur.columns],axis=1)
+
+DataClient = pd.concat([DataClient[col].astype(str).str.lower() for col in DataClient.columns],axis=1)
+DataClient.to_csv('test1.csv')
 #https://www.liquidweb.com/kb/how-to-setup-a-python-virtual-environment-on-ubuntu-18-04/
